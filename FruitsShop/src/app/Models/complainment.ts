877e7962668d7fa801cf6ff793174f8f4a1236db
mohamed <mohamed9abdullah@gmail.com>
2018-08-06 11:17:26 +0200

    using angular6

--- FruitsShop/src/app/Models/complainment.ts
+++
@@ -1,5 +0,0 @@
-export class Complainment {
-
-constructor(public Name:string,public Email:string,public Phone:number,public Complain:string){}
-
-}
--- FruitsShop/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  addProductUrl:"http://localhost:5200/AddProduct/",
-  listProductUrl:"http://localhost:5200/ListProducts",
-  updateProductUrl:"http://localhost:5200/UpdateProduct/",
-  deleteProductUrl:"http://localhost:5200/DeleteProduct/"
-};
